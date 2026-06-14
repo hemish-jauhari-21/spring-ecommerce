c2686d35fc4ca04af52137fd62ba26379f8cbc4f
hemish-jauhari-21 <hemishjauhari@gmail.com>
2026-06-15 00:19:25 +0530

    Added User CRUD APIs

---
+++ src/main/java/com/demo/ecommerce/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.demo.ecommerce.controller;
+
+import com.demo.ecommerce.dto.UserDTO;
+import com.demo.ecommerce.model.User;
+import com.demo.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/ecommerce/user")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/add")
+    public User createUser(@RequestBody UserDTO request) {
+        return service.saveUser(request);
+    }
+
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return service.getUserById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        return service.deleteById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody UserDTO request) {
+        return service.updateUser(id, request);
+    }
+}
--- src/main/java/com/demo/ecommerce/service/UserService.java
+++ src/main/java/com/demo/ecommerce/service/UserService.java
@@ -6,6 +6,9 @@ import com.demo.ecommerce.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -18,5 +21,35 @@ public class UserService {
         newUser.setEmail(request.getEmail());
 
         return repository.save(newUser);
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public String deleteById(Long id) {
+        Optional<User> user = repository.findById(id);
+
+        if (user.isEmpty()) {
+            return "User not present..";
+        }
+
+        repository.deleteById(id);
+        return "User with id: " + id + " deleted..";
+    }
+
+    public User updateUser(Long id, UserDTO request) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        return repository.save(user);
     }
 }
